42125dffc62cb6ad10afafc4e9ebb6a2199bdc21
Tryggvig <tryggvi89@gmail.com>
2018-08-28 23:44:15 +0000

    first publish

---
+++ main.js
@@ -0,0 +1,14 @@
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling: true,
+    scrollHorizontally: false,
+    scrollOverflow: true,
+    navigation: true,
+    fixedElements: '#nav',
+    paddingBottom: '3em',
+    anchors: ['firstPage', 'secondPage', 'thirdPage'],
+    licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+});
+
+//methods
+//fullpage_api.setAllowScrolling(false);
