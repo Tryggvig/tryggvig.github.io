1a12c5cbb09eb761c5d3232d99009413143d3333
Tryggvig <tryggvi89@gmail.com>
2018-08-31 15:40:37 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,1 @@
-new fullpage('#fullpage', {
-    //options here
-    autoScrolling: true,
-    scrollHorizontally: false,
-    scrollOverflow: true,
-    navigation: true,
-    fixedElements: '#nav',
-    paddingBottom: '3em',
-    anchors: ['firstPage', 'secondPage', 'thirdPage'],
-    licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
-});
 
-//methods
-//fullpage_api.setAllowScrolling(false);
